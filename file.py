1765b6be8563779e3999ae75db7d900d659edf55
Kassim Benhaddad <kassim.benhaddad@simplybusiness.co.uk>
2020-11-19 17:24:53 +0000

    Tweaks

--- file.py
+++ file.py
@@ -7,6 +7,7 @@ import numpy as np
 import aubio
 import aiohttp
 import asyncio
+from random import randint
 
 
 # Set up MQTT client
@@ -23,54 +24,17 @@ colour3 = "0,0,255"
 last_value = colour1
 
 colour_values = [
-[50,'174,0,0'],
-[59,'255,0,0'],
-[68,'255,0,0'],
-[77,'255,102,0'],
-[86,'255,239,0'],
-[95,'153,255,0'],
-[104,'40,255,0'],
-[113,'0,255,242'],
-[122,'0,122,255'],
-[131,'5,0,255'],
-[140,'71,0,237'],
-[149,'99,0,178'],
-[158,'174,0,0'],
-[167,'255,0,0'],
-[176,'255,0,0'],
-[185,'255,102,0'],
-[194,'255,239,0'],
-[203,'153,255,0'],
-[212,'40,255,0'],
-[221,'0,255,242'],
-[230,'0,122,255'],
-[239,'5,0,255'],
-[248,'71,0,237'],
-[257,'99,0,178'],
-[266,'174,0,0'],
-[275,'255,0,0'],
-[284,'255,0,0'],
-[293,'255,102,0'],
-[302,'255,239,0'],
-[311,'153,255,0'],
-[320,'40,255,0'],
-[329,'0,255,242'],
-[338,'0,122,255'],
-[347,'5,0,255'],
-[356,'71,0,237'],
-[365,'99,0,178'],
-[374,'174,0,0'],
-[383,'255,0,0'],
-[392,'255,0,0'],
-[401,'255,102,0'],
-[410,'255,239,0'],
-[419,'153,255,0'],
-[428,'40,255,0'],
-[437,'0,255,242'],
-[446,'0,122,255'],
-[455,'5,0,255'],
-[464,'71,0,237'],
-[473,'99,0,178'],
+    {"colour": "dark red", "value": "174,0,0"},
+    {"colour": "red", "value": "255,0,0"},
+    {"colour": "orange-red", "value": "255,102,0"},
+    {"colour": "yellow", "value": "255,239,0"},
+    {"colour": "chartreuse", "value": "153,255,0"},
+    {"colour": "lime", "value": "40,255,0"},
+    {"colour": "aqua", "value": "0,255,242"},
+    {"colour": "sky blue", "value": "0,122,255"},
+    {"colour": "blue", "value": "5,0,255"},
+    {"colour": "blue", "value": "71,0,237"},
+    {"colour": "indigo", "value": "99,0,178"},
 ]
 
 # def ranges(value):
@@ -82,15 +46,18 @@ colour_values = [
 # ranges(24)
 # quit()
 
+
 def change_colour(value):
     # for i in colour_values:
     #     if value < i[0]:
     #         # print (i[1])
     #         return i[1]
-    one= np.random.randint(0,255)
-    two= np.random.randint(0,255)
-    three= np.random.randint(0,255)
-    return (f"{one},{two},{three}")
+    # one= np.random.randint(0,255)
+    # two= np.random.randint(0,255)
+    # three= np.random.randint(0,255)
+    number = randint(0, len(colour_values) - 1)
+    return colour_values[number]
+    # return (f"{one},{two},{three}")
 
     # one= "255,0,0"
     # two= "0,255,0"
@@ -103,6 +70,7 @@ def change_colour(value):
     # elif l == 2:
     #     return three
 
+
 # initialise pyaudio
 p = pyaudio.PyAudio()
 
@@ -111,16 +79,18 @@ buffer_size = 512
 pyaudio_format = pyaudio.paFloat32
 n_channels = 1
 samplerate = 44100
-stream = p.open(format=pyaudio_format,
-                channels=n_channels,
-                rate=samplerate,
-                input=True,
-                frames_per_buffer=buffer_size)
+stream = p.open(
+    format=pyaudio_format,
+    channels=n_channels,
+    rate=samplerate,
+    input=True,
+    frames_per_buffer=buffer_size,
+)
 
 # setup pitch
 tolerance = 0.8
-win_s = 1024 # fft size
-hop_s =  buffer_size # hop size
+win_s = 1024  # fft size
+hop_s = buffer_size  # hop size
 a_tempo = aubio.tempo("default", win_s, hop_s, samplerate)
 a_pitch = aubio.pitch("default", win_s, hop_s, samplerate)
 a_pitch.set_unit("Hz")
@@ -128,6 +98,7 @@ a_pitch.set_tolerance(tolerance)
 
 print("*** starting recording")
 
+last_colour = ""
 while True:
     try:
         audiobuffer = stream.read(buffer_size)
@@ -135,11 +106,27 @@ while True:
         is_beat = a_tempo(signal)
         # print(a_pitch(signal))
         pitch = a_pitch(signal)[0]
-        if is_beat and pitch > 0:
+        if is_beat[0]:
             print(pitch)
-            colour = change_colour(pitch)
-            client.publish("zigbee2mqtt/LED Strip/set", json.dumps({"state" : "ON", "transition": 0.001, "color":{"rgb": colour}}))
-            client.publish(topic, colour)
+            colour_dict = change_colour(pitch)
+            while colour_dict["value"] == last_colour:
+                colour_dict = change_colour(pitch)
+            print(f"Setting colour to {colour_dict['colour']}")
+            client.publish(
+                "zigbee2mqtt/LED Strip/set",
+                json.dumps(
+                    {
+                        "state": "ON",
+                        "brightness": 255,
+                        "transition": 0.001,
+                        "color": {"rgb": colour_dict["value"]},
+                    }
+                ),
+            )
+            client.publish(topic, colour_dict["value"])
+            # client.publish("cmnd/tvlamp/Color2", colour_dict["value"])
+            # client.publish("cmnd/tvlamp2/Color2", colour_dict["value"])
+            last_colour = colour_dict["value"]
 
     except KeyboardInterrupt:
         print("*** Ctrl+C pressed, exiting")
